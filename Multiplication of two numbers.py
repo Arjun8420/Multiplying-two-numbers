6f7890e7472ecfcb054e3cb8c63f1162f9a853cf
Arjun M <90825849+Arjun8420@users.noreply.github.com>
2021-10-02 15:41:50 +0530

    Add files via upload

---
+++ Multiplication of two numbers.py
@@ -0,0 +1,5 @@
+#Multiplication of two numbers
+a = float(input("Enter first number for multiplication = "))
+b = float(input("Enter second number for multiplication = "))
+c = a * b
+print("Answer is = ",c)
